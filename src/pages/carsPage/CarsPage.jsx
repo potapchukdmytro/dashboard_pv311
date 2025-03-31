2ecb2df6542026d2d6548e5e868d83d28f9c7815
potapchukdmytro <traiget@gmail.com>
2025-03-31 19:16:30 +0300

    cars pagination

--- src/pages/carsPage/CarsPage.jsx
+++ src/pages/carsPage/CarsPage.jsx
@@ -2,26 +2,37 @@ import CarCard from "../../components/cards/CarCard";
 import Grid from "@mui/material/Grid2";
 import {useEffect, useState} from "react";
 import axios from "axios";
+import {Pagination, Box} from "@mui/material";
 
 const CarsPage = () => {
+    const [pagination, setPagination] = useState({
+        page: 1,
+        pageSize: 3,
+        pageCount: 1
+    });
     const [cars, setCars] = useState([]);
 
-    const apiUrl = "https://localhost:7220/api/car/list";
+    const changePageHandler = (event, value) => {
+        setPagination({...pagination, page: value});
+    }
 
     const fetchCars = async () => {
+        const apiUrl = `https://localhost:7220/api/car/list?page=${pagination.page}&pageSize=${pagination.pageSize}`;
         const response = await axios.get(apiUrl);
         if (response.status === 200) {
             const data = response.data;
-            setCars(data.payload);
+            setCars(data.payload.cars);
+            setPagination({...pagination, page: data.payload.page, pageCount: data.payload.pageCount});
         }
     }
 
     useEffect(() => {
         fetchCars()
             .catch(error => console.log(error));
-    }, [])
+    }, [pagination.page])
 
     return (
+        <>
         <Grid container spacing={2} sx={{mt: 2}}>
             {
                 cars.map((car) => (
@@ -29,7 +40,12 @@ const CarsPage = () => {
                         <CarCard car={car}/>
                     </Grid>
                 ))}
+
         </Grid>
+        <Box display="flex" mt={2} justifyContent="center">
+            <Pagination page={pagination.page} onChange={changePageHandler} count={pagination.pageCount}/>
+        </Box>
+        </>
     )
 }
 
